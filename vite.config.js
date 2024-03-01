2186737be1aa3ec9e07dea26b8d01f5cdc149169
en-vang <enrique@si.team>
2024-02-29 23:29:28 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = process.env.VITE_ASSET_PATH || '/';
+
 export default defineConfig({
   plugins: [react()],
+  base,
   // base: '/project-name/',
-  base: process.env.VITE_ASSET_PATH, 
+  // base: process.env.VITE_ASSET_PATH, 
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
