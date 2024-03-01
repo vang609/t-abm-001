c24ed0c5032ae2f143b06c5ce4ff085787f032ee
en-vang <enrique@si.team>
2024-02-29 23:22:34 -0500

    vite config with base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // base: '/project-name/',
   base: process.env.VITE_ASSET_PATH, 
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
