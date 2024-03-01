c357d8df45bcc7742dc9346f9e9ddb0373c45ae9
en-vang <enrique@si.team>
2024-02-29 22:30:07 -0500

    vite config preparation

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/t-abm-001/', 
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
