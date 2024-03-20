19b8214d208aae3b22de7aa6b9b9bb721eec8726
en-vang <enrique@si.team>
2024-03-20 00:16:18 -0400

    Fixing carousle image

--- src/widgets/carousel/Carousel.jsx
+++ src/widgets/carousel/Carousel.jsx
@@ -5,34 +5,28 @@ export const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const slideInterval = useRef();
 
-  // Function to go to next slide
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  // Function to go to a specific slide
   const goToSlide = (index) => {
     setCurrentIndex(index);
-    // Reset and restart the interval when manually changing slides
     clearInterval(slideInterval.current);
     slideInterval.current = setInterval(nextSlide, 3000);
   };
 
-  // Set up the interval for automatic sliding
   useEffect(() => {
-    slideInterval.current = setInterval(nextSlide, 3000); // Change slide every 3 seconds
-
-    // Clear interval on component unmount
+    slideInterval.current = setInterval(nextSlide, 3000);
     return () => clearInterval(slideInterval.current);
   }, [currentIndex, images.length]);
 
   return (
-    <Card className="carousel-card" style={{ borderRadius: '20px', overflow: 'hidden', position: 'relative', width: '100%', maxWidth: '1200px', margin: 'auto' }}> {/* Adjust width as needed */}
-      <div className="carousel" style={{ overflow: 'hidden', width: '100%', height: '600px' }}> {/* Increase height as needed */}
+    <Card className="carousel-card mx-3 mt-20 mb-6 lg:mx-4 border border-blue-gray-100" style={{ overflow: 'hidden', position: 'relative', width: '100%', maxWidth: '1200px', margin: 'auto', height: 'auto' }}>
+      <div className="carousel" style={{ overflow: 'hidden', width: '100%', height: 'auto' }}>
         <div className="carousel-inner" style={{ display: 'flex', transition: 'transform 0.5s ease', transform: `translateX(-${currentIndex * 100}%)` }}>
           {images.map((item, index) => (
-            <div className="carousel-item" key={index} style={{ minWidth: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <img src={`${import.meta.env.VITE_APP_ASSET_PATH}${item.img}`} alt={`Slide ${index}`} style={{ width: 'auto', height: '100%', objectFit: 'contain' }} />
+            <div className="carousel-item" key={index} style={{ minWidth: '100%', height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <img src={`${import.meta.env.VITE_APP_ASSET_PATH}${item.img}`} alt={`Slide ${index}`} style={{ width: '100%', height: 'auto', objectFit: 'cover' }} />
             </div>
           ))}
         </div>
